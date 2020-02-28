08be164b56b7d632108cc9279abb0ce43c88f5c7
remcoa <r.aalbers@science.ru.nl>
2020-02-28 14:14:47 +0100

    add sorting by key for toolbar elements

--- src/Options.php
+++ src/Options.php
@@ -363,7 +363,7 @@ class Options extends AbstractOptions
 
                         $this->toolbar[$key][$collector] = $template;
                     }
-
+                    ksort($this->toolbar[$key]);
                     break;
                 default:
                     // Unknown type; ignore
